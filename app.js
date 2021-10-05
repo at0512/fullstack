30a1b0899e7781e4b7600fbf16a9782576c35c18
athul <absathulraj@gmail.com>
2021-10-05 19:45:20 +0530

    primitive data type

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log("Hello world") 
+console.warn("Hello Warning") 
+console.error("Hello error") 
+console.log(typeof 55) 
+//numbers 
+console.log(typeof -1.3)
+console.log(typeof 3)
+console.log(typeof 'sgr')
+console.log(typeof 3)
+console.log(typeof 4)
+console.log(typeof 12)
+console.log(typeof 13)
+
