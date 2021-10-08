935f9d54ac6d87e8ecf7c2704ee1dac88466e2be
athul <absathulraj@gmail.com>
2021-10-08 10:10:46 +0530

    js

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
-// function greet(name){ 
-//     console.log(name)
+var list = document.querySelector('ul')
+var input  = document.querySelector('input')
+var button = document.querySelector('button') 
+ const callbackfnc = (eve) =>{
+     console.log(eve.value)
+     console.log(eve.target)
+ }
 
-//     console.log('hello')
-// // } 
-// function aj(name='john', age='20'){ 
-//     console.log("hey " +name+ " your age is " +age)
-// }
-// greet('athul') 
-// aj()
-// var sum = (a=0,b=2) =>{ 
-//     var add =a+b
-//     return add
+button.addEventListener('click', callbackfnc)
 
-// } 
-// var result = sum(10+20) 
-// console.log(result) 
-var sub = (a=0, b=0) => a-b 
-//this is a shortcut to calling a function and return 
-console.log(sub(12, 4))
+
