abc880b2513900d46d88cc543cfdf26bbec5fbaa
athul <absathulraj@gmail.com>
2021-10-08 19:10:03 +0530

    calculator

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
-var list = document.querySelector('ul')
-var body = document.querySelector('body')
+const a = document.querySelectorAll('input')[0]
+const b = document.querySelectorAll('input')[1] 
+const add = document.querySelector('#add')
+const subract = document.querySelector('#sub') 
+const result = document.querySelector('.result') 
+// const addition = ()=>{
+//    const results = parseInt(a.value) + parseInt(b.value) 
+//    console.log(results) 
+//    result.innerHTML = results
+// } 
+// const subractiom = ()=>{
+//     const results = parseInt(a.value) - parseInt(b.value) 
+//     console.log(results) 
+//     result.innerHTML = results
+//  } 
+const fun = (event, operation)=>{
+   if(operation=='add'){ 
+      console.log(event.target)
+      output = parseInt(a.value) + parseInt(b.value) 
+      result.innerHTML = output  
+   } 
+   else {
+      output = parseInt(a.value) * parseInt(b.value) 
+      result.innerHTML = output
+   }
+}
 
-var input  = document.querySelector('input')
-var button = document.querySelector('button') 
- const callbackfnc = (event) =>{ 
-    //  const inputvalue = input.value
-    //  const element = document.createElement('li')
-    //  const textnode = document.createTextNode(inputvalue) 
-    //  element.appendChild(textnode) 
-    //  list.appendChild(element) 
-    body.classList.toggle('dark')
-
- }
-
-button.addEventListener('click', callbackfnc)
-
-
+add.addEventListener('click', (event) =>{
+  return fun(event, 'add')
+}) 
+subract.addEventListener('click', (event)=> {
+   return fun(subract)
+})
