66301ab4281700eab3679cbfe81f83b0c9dce1a1
athul <absathulraj@gmail.com>
2021-10-08 14:55:50 +0530

    dark

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 var list = document.querySelector('ul')
+var body = document.querySelector('body')
+
 var input  = document.querySelector('input')
 var button = document.querySelector('button') 
  const callbackfnc = (event) =>{ 
-     console.log(event.target) 
-     console.log(input.value)
+    //  const inputvalue = input.value
+    //  const element = document.createElement('li')
+    //  const textnode = document.createTextNode(inputvalue) 
+    //  element.appendChild(textnode) 
+    //  list.appendChild(element) 
+    body.classList.toggle('dark')
+
  }
- 
 
 button.addEventListener('click', callbackfnc)
 
